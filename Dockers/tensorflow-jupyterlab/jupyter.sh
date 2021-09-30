71b8eb7f0c96715fadbaddf88ac4c90f645eb7cb
Yûki Vachot <yuki@vachot.fr>
2021-09-30 23:55:35 +0200

    Add files via upload

--- Dockers/tensorflow-jupyterlab/jupyter.sh
+++ Dockers/tensorflow-jupyterlab/jupyter.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -eu
 
-jupyter lab --ip=0.0.0.0 --port=8080 --allow-root --collaborative \
+jupyter lab --ip=0.0.0.0 --port=8080 --no-browser --allow-root --collaborative \
   --ServerApp.token='' \
-  --ServerApp.custom_display_url=${JOB_URL_SCHEME}${JOB_ID}-8080.${JOB_HOST} \
+  --ServerApp.custom_display_url=${JOB_URL_SCHEME-jobUrl}${JOB_ID-jobId}-8080.${JOB_HOST-jobHost} \
   --ServerApp.allow_remote_access=True \
   --ServerApp.allow_origin='*' \
-  --ServerApp..disable_check_xsrf=True
+  --ServerApp.disable_check_xsrf=True
